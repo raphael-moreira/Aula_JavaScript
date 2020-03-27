66cdd2f0cbdf65d6f18bdff64363e5d40f94a2d0
Raphael Moreira <61034183+raphael-moreira@users.noreply.github.com>
2020-03-26 22:02:57 -0300

    Create funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+alert("Aula JavaScript");
+//comentário
+/*linha 1
+linha 2
+linha 3
+varias linhas
+...*/
+var numero = 2020;
+var nome = "Raphael";
+var carac = '';
+var x = 4.5;
+var y = 5;
+alert(x + y);
